5aee559104298a3f1858e33b703df4a7e79b1a65
JeffBuck8 <jdbuckcode@gmail.com>
2022-08-25 13:36:44 -0400

    fixed server issues, have to runserver 127.0.0.1:9000

--- django_music/settings.py
+++ django_music/settings.py
@@ -123,3 +123,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# INTERNAL_IPS = [
+#     "127.0.0.1:9000",
+# ]
